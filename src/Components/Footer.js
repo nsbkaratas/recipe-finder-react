757869c65d04698dd65fc600d2453d8c8986be1f
Nesibe Karatas <nsbkaratas@gmail.com>
2021-09-16 00:14:28 -0400

    Images for footer added

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import logo from "C:/Users/nsbka/Desktop/Homework/Projects/mod2-project/src/Images/facebook.png"
+
 
 const Footer = () => {
     return (
-        <div>
-            
+        <div className="footer">
+            <h3>Purr-fect Foods</h3>
+            <p>Follow the Purr-fect Foods to become the purr-fect masterchef!</p>
+            <ul>
+                <li><a href=""><img src={logo} alt="facebook" /></a></li>
+                <li><a href="#"><i className="twitter"></i></a></li>
+                <li><a href="#"><i className="instagram"></i></a></li>
+            </ul>
         </div>
     )
 }
